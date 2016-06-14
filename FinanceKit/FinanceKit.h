20d623325718d81bf74a4f1f1d4c7c8f5c7eded1
Christian Mitteldorf <christian.km@me.com>
2016-06-14 19:23:30 +0200

    Initial Commit

---
+++ FinanceKit/FinanceKit.h
@@ -0,0 +1,19 @@
+//
+//  FinanceKit.h
+//  FinanceKit
+//
+//  Created by Christian Mitteldorf on 14/06/2016.
+//  Copyright © 2016 Christian Mitteldorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FinanceKit.
+FOUNDATION_EXPORT double FinanceKitVersionNumber;
+
+//! Project version string for FinanceKit.
+FOUNDATION_EXPORT const unsigned char FinanceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FinanceKit/PublicHeader.h>
+
+
