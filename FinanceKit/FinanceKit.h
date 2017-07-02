0c6a9dfdb6cb9618f81f4b63bdc1e10263fdd2a9
Christian Mitteldorf <christian.km@me.com>
2017-07-02 18:00:07 +0200

    Added .gitignore

--- FinanceKit/FinanceKit.h
+++ FinanceKit/FinanceKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Christian Mitteldorf. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for FinanceKit.
 FOUNDATION_EXPORT double FinanceKitVersionNumber;
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double FinanceKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char FinanceKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FinanceKit/PublicHeader.h>
-
-
