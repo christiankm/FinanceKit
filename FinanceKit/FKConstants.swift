d58fab9b8c9958981052725b95d4bfdedbf6b591
Christian Mitteldorf <christian.km@me.com>
2017-07-02 18:02:37 +0200

    Added FKConstants containing global constants

---
+++ FinanceKit/FKConstants.swift
@@ -0,0 +1,19 @@
+//
+//  FKConstants.swift
+//  FinanceKit
+//
+//  Created by Christian Mitteldorf on 16/06/2016.
+//  Copyright © 2016 Christian Mitteldorf. All rights reserved.
+//
+
+import Foundation
+
+struct FKConstants {
+
+    struct YahooAPI {
+        static let BaseUrl = "https://query.yahooapis.com/v1/public/yql"
+        static let TableFinanceQuotes = "yahoo.finance.quotes"
+        static let TableFInanceDividendHistory = "yahoo.finance.dividendhistory"
+    }
+    
+}
