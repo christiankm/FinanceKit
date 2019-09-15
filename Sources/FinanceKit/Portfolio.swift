037ca3a3ea11a9bef879bf18361ecdcd93c84d90
Christian Mitteldorf <christian.km@me.com>
2019-09-15 02:21:39 +0200

    Create simple struct for a Portfolio

---
+++ Sources/FinanceKit/Portfolio.swift
@@ -0,0 +1,16 @@
+//
+//  Portfolio.swift
+//  FinanceKit
+//
+//  Created by Christian Mitteldorf on 15/09/2019.
+//  Copyright © 2017 Christian Mitteldorf. All rights reserved.
+//
+
+import Foundation
+
+public struct Portfolio: Codable, Identifiable {
+    public let id: UUID = UUID()
+    public let name: String
+    public let currency: Currency
+    public var stocks: [Stock] = []
+}
