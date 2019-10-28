8401b5219c515f54e5b3db160ce3104cbc4550a6
Christian Mitteldorf <christian.km@me.com>
2019-10-28 18:59:18 +0100

    Add Transaction

---
+++ Sources/FinanceKit/Transaction.swift
@@ -0,0 +1,37 @@
+//
+//  Transaction.swift
+//  FinanceKit
+//
+//  Created by Christian Mitteldorf on 19/10/2019.
+//  Copyright © 2019 Christian Mitteldorf. All rights reserved.
+//
+
+import Foundation
+
+public enum TransactionType: Int, Codable {
+    case buy
+    case sell
+    // TODO: Add support for dividends
+//    case dividend
+}
+
+public struct Transaction: Codable {
+
+    public let type: TransactionType
+    public let symbol: Symbol
+    public let date: Date
+    public let price: Double
+    public let quantity: Int
+
+    public var transactionCost: Decimal {
+        Decimal(Double(quantity) * price)
+    }
+
+    public init(type: TransactionType, symbol: Symbol, date: Date, price: Double, quantity: Int) {
+        self.type = type
+        self.symbol = symbol
+        self.date = date
+        self.price = price
+        self.quantity = quantity
+    }
+}
