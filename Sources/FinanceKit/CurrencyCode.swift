87d5f45f8bb4f5999de895eddf99b6cfa59c48b4
Christian Mitteldorf <christian@mitteldorf.dk>
2019-11-30 13:22:50 +0100

    Extract CurrencyCode to seperate file and add test file

---
+++ Sources/FinanceKit/CurrencyCode.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyCode.swift
+//  FinanceKit
+//
+//  Created by Christian Mitteldorf on 29/11/2019.
+//
+
+import Foundation
+
+public enum CurrencyCode: String, CaseIterable, Codable {
+    case unknown
+    case DKK
+    case USD
+    case EUR
+    case GBP
+    case AUD
+    case NOK
+    case SEK
+
+    public var currencyCodeString: String {
+        rawValue.uppercased()
+    }
+}
---
+++ Tests/FinanceKitTests/CurrencyCodeTests.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyCodeTests.swift
+//  FinanceKitTests
+//
+//  Created by Christian Mitteldorf on 30/11/2019.
+//
+
+import XCTest
+
+class CurrencyCodeTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
