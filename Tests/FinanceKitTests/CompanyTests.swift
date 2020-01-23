a6d965d932da61d1ca08e7f8c271c77dc47b8215
Christian Mitteldorf <christian@mitteldorf.dk>
2020-01-23 17:11:47 +0100

    Add tests and fix problems for 100% coverage

---
+++ Tests/FinanceKitTests/CompanyTests.swift
@@ -0,0 +1,55 @@
+//
+//  CompanyTests.swift
+//  FinanceKitTests
+//
+//  Created by Christian Mitteldorf on 23/01/2020.
+//
+
+import XCTest
+import FinanceKit
+
+class CompanyTests: XCTestCase {
+
+    func testInit() {
+        let apple = Company(
+            symbol: Symbol("AAPL")!, //swiftlint:disable:this force_unwrapping
+            name: "Apple Inc.",
+            currency: Currency(code: .USD)
+        )
+
+        XCTAssertEqual(apple.symbol, Symbol("AAPL")!) //swiftlint:disable:this force_unwrapping
+        XCTAssertEqual(apple.name, "Apple Inc.")
+        XCTAssertEqual(apple.currency, Currency(code: .USD))
+    }
+
+    func testEquatable() {
+        let apple = Company(
+            symbol: Symbol("AAPL")!, //swiftlint:disable:this force_unwrapping
+            name: "Apple Inc.",
+            currency: Currency(code: .USD)
+        )
+        let coke = Company(
+            symbol: Symbol("KO")!, //swiftlint:disable:this force_unwrapping
+            name: "Coca-Cola",
+            currency: Currency(code: .USD)
+        )
+
+        XCTAssertEqual(apple, apple)
+        XCTAssertNotEqual(apple, coke)
+    }
+
+    func testComparable() {
+        let apple = Company(
+            symbol: Symbol("AAPL")!, //swiftlint:disable:this force_unwrapping
+            name: "Apple Inc.",
+            currency: Currency(code: .USD)
+        )
+        let coke = Company(
+            symbol: Symbol("KO")!, //swiftlint:disable:this force_unwrapping
+            name: "Coca-Cola",
+            currency: Currency(code: .USD)
+        )
+
+        XCTAssertEqual([coke, apple].sorted(), [apple, coke])
+    }
+}
