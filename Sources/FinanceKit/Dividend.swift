f46787cd4c7f8d6863ebef91ed14e09055b159f0
Christian Mitteldorf <christian.km@me.com>
2020-03-14 10:51:22 +0100

    Add simple Dividend model

---
+++ Sources/FinanceKit/Dividend.swift
@@ -0,0 +1,14 @@
+//
+//  Dividend.swift
+//  FinanceKit
+//
+//  Created by Christian Mitteldorf on 05/08/2017.
+//
+
+import Foundation
+
+public struct Dividend: Hashable, Identifiable, Equatable {
+    public let id = UUID()
+    public let symbol: Symbol
+    public var value: Amount = 0
+}
