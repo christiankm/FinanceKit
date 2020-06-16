7287dcdf4893487049a03efc1b9b8caed08f3eb3
Christian Mitteldorf <christian@mitteldorf.dk>
2020-06-16 15:20:57 +0200

    Update Swift Package Language versions to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,5 +25,6 @@ let package = Package(
         .testTarget(
             name: "FinanceKitTests",
             dependencies: ["FinanceKit"])
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
