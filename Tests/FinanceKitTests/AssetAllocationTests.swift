503a7273563c166e44c7423e6e4d98d977d3bf4a
Christian Mitteldorf <christian@mitteldorf.dk>
2021-11-21 13:25:20 +1100

    Add model for AssetAllocation

---
+++ Tests/FinanceKitTests/AssetAllocationTests.swift
@@ -0,0 +1,57 @@
+//
+//  AssetAllocationTests.swift
+//  FinanceKitTests
+//
+//  Created by Christian Mitteldorf on 24/10/2021.
+//
+
+import FinanceKit
+import XCTest
+
+class AssetAllocationTests: XCTestCase {
+
+    func testInit() throws {
+        let sut = AssetAllocation(
+            name: "Stocks",
+            allocation: Percentage(0.3),
+            returnRate: Percentage(0.09)
+        )
+
+        XCTAssertEqual(sut.name, "Stocks")
+        XCTAssertEqual(sut.allocation.decimal, 0.3)
+        XCTAssertEqual(sut.returnRate.decimal, 0.09)
+    }
+
+    func testAverageReturnRate() throws {
+        let emptyAllocations = [AssetAllocation]()
+        XCTAssertEqual(emptyAllocations.averageReturnRate, .zero)
+
+        let oneAllocation = [
+            AssetAllocation(
+                name: "Stocks",
+                allocation: Percentage(0.3),
+                returnRate: Percentage(0.09)
+            )
+        ]
+        XCTAssertEqual(oneAllocation.averageReturnRate, Percentage(0.027))
+
+        let multipleAllocations = [
+            AssetAllocation(
+                name: "Stocks",
+                allocation: Percentage(0.7),
+                returnRate: Percentage(0.11)
+            ),
+            AssetAllocation(
+                name: "Bonds",
+                allocation: Percentage(0.1),
+                returnRate: Percentage(0.02)
+            ),
+            AssetAllocation(
+                name: "Cash",
+                allocation: Percentage(0.2),
+                returnRate: Percentage(-0.03)
+            )
+        ]
+        XCTAssertEqual(multipleAllocations.averageReturnRate, Percentage(0.073))
+    }
+}
